7a7df3debd8a153d8b1acae6addb6ab583c9416a
Lucas Kawatoko <lucaskawatoko@gmail.com>
2025-12-01 21:35:15 -0300

    adiciona limpeza de tema na ativação da extensão

--- extension.js
+++ extension.js
@@ -1,13 +1,24 @@
 const vscode = require("vscode");
 const FeatureManager = require("./src/core/featureManager");
 const featuresList = require("./src/index"); 
+const ThemeCleanup = require("./src/core/themeCleanup"); // <--- IMPORTAMOS DIRETO
 
 let manager;
 
-function activate(context) {
+async function activate(context) {
     console.log("HyperDark: Iniciando...");
 
-    // O FeatureManager pega a lista e ativa um por um
+    // --- A VACINA (Rodando manualmente para garantir) ---
+    try {
+        console.log("HyperDark: Tentando rodar limpeza de tema...");
+        const cleaner = new ThemeCleanup(context);
+        await cleaner.activate(); // Força a execução agora
+    } catch (e) {
+        console.error("HyperDark: Falha na vacina:", e);
+    }
+    // ----------------------------------------------------
+
+    // O FeatureManager continua cuidando do resto (Relógio, Servidor, etc)
     manager = new FeatureManager(context, featuresList);
     manager.activateAll();
 }
